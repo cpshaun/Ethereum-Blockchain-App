36b41ef767efa2c7bd95c6f688407c2078ea6c53
Shaun__ <35448957+cpshaun@users.noreply.github.com>
2023-01-17 08:40:49 +0800

    updated packages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 
 // const apiKey = import.meta.env.VITE_API_KEY;
-const apiKey = '369b67875091469c84fd0eb58b2395c7'
+// const apiKey = '369b67875091469c84fd0eb58b2395c7'
 
-const Web3 = require('web3')
-const provider = 'https://mainnet.infura.io/v3/'+apiKey
-const web3Provider = new Web3.providers.HttpProvider(provider)
-const web3 = new Web3(web3Provider);
-web3.eth.getBlockNumber().then((res) => {
-    console.log("Latest Ethereum Block is ",res);
-})
+// const Web3 = require('web3')
+// const provider = 'https://mainnet.infura.io/v3/'+apiKey
+// const web3Provider = new Web3.providers.HttpProvider(provider)
+// const web3 = new Web3(web3Provider);
+// web3.eth.getBlockNumber().then((res) => {
+//     console.log("Latest Ethereum Block is ",res);
+// })
